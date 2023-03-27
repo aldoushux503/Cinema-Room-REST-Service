add9b767b491bb3396aa1a1a11882ed3dc9af356
aldoushux503 <elnikovnikita4@gmail.com>
2023-03-27 17:24:25 +0200

    Add new endpoint /purchase in class CinemaController and parse POST JSON body

--- src/main/java/com/example/cinemaroomrestservice/CinemaController.java
+++ src/main/java/com/example/cinemaroomrestservice/CinemaController.java
@@ -2,17 +2,38 @@ package com.example.cinemaroomrestservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CinemaController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CinemaRoomRestServiceApplication.class);
     private final Cinema cinema = new Cinema();
 
     @GetMapping("/seats")
-    public Cinema getCinemaInformation() {
+    public Cinema getCinemaInformation(@RequestBody String requestJson) {
         return cinema;
+    }
+
+
+    @PostMapping("/purchase")
+    public Seat purchaseSeat(@RequestBody String requestJson) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        Seat purchase = null;
+        try {
+            purchase = mapper.readValue(requestJson, Seat.class);
+        } catch (JsonProcessingException e) {
+            LOGGER.error(String.valueOf(e));
+        }
+
+
+        return purchase;
     }
 
 }
