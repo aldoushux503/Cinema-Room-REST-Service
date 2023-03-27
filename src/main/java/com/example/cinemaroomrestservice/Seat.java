fd2bea9bb143707a27d111b2f44d13f0d1661a7d
aldoushux503 <elnikovnikita4@gmail.com>
2023-03-27 17:14:25 +0200

    Added field price to class Seat

--- src/main/java/com/example/cinemaroomrestservice/Seat.java
+++ src/main/java/com/example/cinemaroomrestservice/Seat.java
@@ -1,12 +1,19 @@
 package com.example.cinemaroomrestservice;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Seat {
     private final int row;
     private final int column;
 
-    public Seat(int row, int column) {
+    private final int price;
+
+    public Seat(@JsonProperty("row") int row, @JsonProperty("column") int column) {
         this.row = row;
         this.column = column;
+
+        //If the row number <= to 4, the price will be 10, other rows cost 8 per seat.
+        this.price = row <= 4 ? 10 : 8;
     }
 
     public int getRow() {
@@ -15,5 +22,9 @@ public class Seat {
 
     public int getColumn() {
         return column;
+    }
+
+    public int getPrice() {
+        return price;
     }
 }
