fdb772dfa8f76ad2de22c38cb95138747dc20b08
aldoushux503 <elnikovnikita4@gmail.com>
2023-03-27 17:35:53 +0200

    Add ControllerExceptionHandler and CustomErrorMessage classes

---
+++ src/main/java/com/example/cinemaroomrestservice/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.cinemaroomrestservice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(SeatNotFoundException.class)
+    public ResponseEntity<CustomErrorMessage> handleFlightNotFound(
+            SeatNotFoundException e, WebRequest request) {
+
+        CustomErrorMessage body = new CustomErrorMessage("The number of a row or a column is out of bounds!");
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SeatPurchasedException.class)
+    public ResponseEntity<CustomErrorMessage> handleFlightNotFound(
+            SeatPurchasedException e, WebRequest request) {
+
+        CustomErrorMessage body = new CustomErrorMessage("The ticket has been already purchased!");
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
+}
+
---
+++ src/main/java/com/example/cinemaroomrestservice/CustomErrorMessage.java
@@ -0,0 +1,14 @@
+package com.example.cinemaroomrestservice;
+
+public class CustomErrorMessage {
+
+    private String error;
+
+    public CustomErrorMessage(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
