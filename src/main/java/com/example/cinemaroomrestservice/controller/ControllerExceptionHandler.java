6ef445068745556bcbae1f250f7a34b48b939aeb
aldoushux503 <elnikovnikita4@gmail.com>
2023-04-01 18:24:08 +0200

    Deleted unnecessary new lines

--- src/main/java/com/example/cinemaroomrestservice/controller/ControllerExceptionHandler.java
+++ src/main/java/com/example/cinemaroomrestservice/controller/ControllerExceptionHandler.java
@@ -11,28 +11,24 @@ import org.springframework.web.context.request.WebRequest;
 public class ControllerExceptionHandler {
     @ExceptionHandler(SeatNotFoundException.class)
     public ResponseEntity<CustomErrorMessage> handleSeatNotFound() {
-
         CustomErrorMessage body = new CustomErrorMessage("The number of a row or a column is out of bounds!");
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(SeatPurchasedException.class)
     public ResponseEntity<CustomErrorMessage> handleTicketPurchased() {
-
         CustomErrorMessage body = new CustomErrorMessage("The ticket has been already purchased!");
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(WrongTokenException.class)
     public ResponseEntity<CustomErrorMessage> handleWrongToken() {
-
         CustomErrorMessage body = new CustomErrorMessage("Wrong token!");
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(WrongPasswordException.class)
     public ResponseEntity<CustomErrorMessage> handleWrongPassword() {
-
         CustomErrorMessage body = new CustomErrorMessage("The password is wrong!");
         return new ResponseEntity<>(body, HttpStatus.UNAUTHORIZED);
     }
